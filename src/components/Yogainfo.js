b41f04a1c239419e62204b2e07c815d26a309cb6
Lizbeth Lage <llage.bergen@gmail.com>
2024-10-02 16:56:26 -0400

    Add Yogainfo.js

---
+++ src/components/Yogainfo.js
@@ -0,0 +1,34 @@
+import Yogaimage from "../assets/images/Yogaimage.png"
+import '../scss/Yogainfo.scss'
+
+export function Yogainfo() {
+    return (
+        <div className= 'Yoga-container'>
+            <div className='infobox'>
+            </div>
+            <img 
+            className="Yogaimage"
+            src={Yogaimage}
+            alt="Yoga image">
+            </img>
+            <div className="Yoga-text">
+            <h1>YOGA</h1>
+            <h4>Dixon Mills</h4>
+            <p>
+            Ease into your weekend every Friday
+            at 8:00 AM with a calming yoga session 
+            located at Dixon Mills. Ideal for 
+            unwinding after a busy week of running 
+            or gearing up for a weekend run or bike 
+            ride. All levels welcome. 
+            </p>
+            <div className='Location-container'>
+            <a href="https://www.google.com/maps/place/341+Monmouth+St,+Jersey+City,+NJ+07302/@40.721358,-74.050008,1289m/data=!3m1!1e3!4m6!3m5!1s0x89c250b4482bb7a5:0x3e630e789958ae0d!8m2!3d40.7213576!4d-74.0500781!16s%2Fg%2F11c5kzn93j?hl=en-US&entry=ttu" target="_blank" rel="noopener">Location</a>
+            </div>
+            </div>
+            </div>
+        
+
+
+    );
+}
