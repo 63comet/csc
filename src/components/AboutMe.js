a5c48b56a22da13aa82d527673b8219344f92b25
Lizbeth Lage <llage.bergen@gmail.com>
2024-10-02 16:58:53 -0400

    Add AboutMe.js

---
+++ src/components/AboutMe.js
@@ -0,0 +1,27 @@
+import { Box } from "@primer/react";
+import Aboutcover from "../assets/images/Aboutcover.png";
+import ABOUT from "../assets/logos/ABOUT.png"
+import '../scss/AboutMe.scss';
+
+
+
+export function AboutMe() {
+  return (
+    <div className='about-me-container'>
+      <img 
+      className="logo"
+      src={ABOUT}
+      alt="ABOUT logo"
+      />  
+
+
+      <img 
+        className="cover-photo" 
+        src={Aboutcover} 
+        alt="About cover"
+      />
+    </div>
+  );
+
+}
+
