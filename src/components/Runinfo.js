6ea6df7d733a34950aff2e02fc4449726bbbcd6b
Lizbeth Lage <llage.bergen@gmail.com>
2024-10-17 12:12:56 -0400

    Add slideshow7.png

--- src/components/Runinfo.js
+++ src/components/Runinfo.js
@@ -1,30 +1,54 @@
+import React, { useState } from "react"; 
 import RunImage from "../assets/images/Runimage.png";
-import '../scss/Runinfo.scss'
+import DepartedImage from "../assets/images/departed.png"; 
+import '../scss/Runinfo.scss';
 
 export function Runinfo() {
+    const [imageSrc, setImageSrc] = useState(RunImage); 
+    const [imageOpacity, setImageOpacity] = useState(1);
     return (
         <div className='Run-container'>
-            <div className= 'infobox'>
-            </div>  
-        <div className="Run-text">
-            <h1>RUN</h1>
-            <p>
-            Every Monday at 6:45 PM, meet
-            at Departed Soles in Jersey City 
-            for a group run. All fitness levels
-            are welcome, with walking pace options 
-            and biking available. Come connect and 
-            enjoy the fun.
-            </p>
-            <div className='Location-container'>
-            <h4>Departed Soles</h4>
-            <a href="https://www.google.com/maps?ll=40.72087,-74.040134&z=15&t=m&hl=en-US&gl=US&mapclient=embed&cid=9782111646039970650" target="_blank" rel="noopener">Location</a>
-            </div>
+            <div className='infobox'></div>
+            <div className="Run-text">
+                <h1>RUN</h1>
+                <p>
+                    Every Monday at 6:45 PM, meet at Departed Soles in Jersey City 
+                    for a group run. All fitness levels are welcome, with walking 
+                    pace options and biking available. Come connect and enjoy the fun.
+                </p>
+                <div className='Location-container'>
+                    <h4>Departed Soles</h4>
+                    <div className='location-wrapper'>
+                        <a 
+                            href="https://www.google.com/maps?ll=40.72087,-74.040134&z=15&t=m&hl=en-US&gl=US&mapclient=embed&cid=9782111646039970650" 
+                            target="_blank" 
+                            rel="noopener"
+                            onMouseEnter={() => {
+                                setImageOpacity(0); // Fade out the current image
+                                setTimeout(() => {
+                                    setImageSrc(DepartedImage); 
+                                    setImageOpacity(1); // Immediately prepare for fade-in
+                                }, 500); // Change the image source after 1 second
+                            }}
+                            onMouseLeave={() => {
+                                setImageOpacity(0); // Fade out the current image
+                                setTimeout(() => {
+                                    setImageSrc(RunImage);
+                                    setImageOpacity(1); // Fade in the original image
+                                }, 500); // Change back to the original image after 1 second
+                            }}
+                            
+                        >
+                            Location
+                        </a>
+                    </div>
+                </div>
             </div>
             <img
                 className="Runimage"
-                src={RunImage} 
+                src={imageSrc} 
                 alt="Run image"
+                style={{opacity: imageOpacity}}
             />
         </div>
     );
--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,11 +1,13 @@
 import slideshow1 from "../assets/images/slideshow1.png";
 import slideshow2 from "../assets/images/slideshow2.png";
 import slideshow4 from "../assets/images/slideshow4.png";
+import slideshow5 from "../assets/images/slideshow5.png";
+import slideshow6 from "../assets/images/slideshow6.png";
 import '../scss/Slideshow.scss';
 
 export function Slideshow() {
     return (
-        <div className='Slideshow-row'> {/* Added row class here */}
+        <div className='Slideshow-row'> 
             <div className='Slideshow-container'>
                 <div className='photos'>
                     <img
@@ -32,6 +34,26 @@ export function Slideshow() {
                         alt="Slideshow 4"
                     />
                 </div>
+            <div className= 'slideshow5-container'>
+                <div className= 'photo'>
+                    <img
+                    className='photo'
+                    src={slideshow5}
+                    alt="slideshow 5">
+
+                    </img>
+                </div>
+            <div className= 'slideshow2-container'>
+                <div className='photo'>
+                    <img 
+                    className='photo'
+                    src={slideshow6}
+                    alt="slideshow 6">
+
+                    </img>
+                </div>
+            </div>
+            </div>
             </div>
         </div>
     );
