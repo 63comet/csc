d1dd30f3eb3fb04aff4eb7e020de238e531d167a
Lizbeth Lage <llage.bergen@gmail.com>
2024-10-22 11:45:38 -0400

    add slideshow8.png

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -3,6 +3,9 @@ import slideshow2 from "../assets/images/slideshow2.png";
 import slideshow4 from "../assets/images/slideshow4.png";
 import slideshow5 from "../assets/images/slideshow5.png";
 import slideshow6 from "../assets/images/slideshow6.png";
+import slideshow7 from "../assets/images/slideshow7.png";
+import slideshow8 from "../assets/images/slideshow8.png";
+
 import '../scss/Slideshow.scss';
 
 export function Slideshow() {
@@ -16,6 +19,17 @@ export function Slideshow() {
                         alt="Slideshow 1"
                     />
                 </div>
+            <div className='Slideshow8-container'>
+                <div className='slideshow8-container'>
+                    <div className='photos'>
+                        <img
+                        className='photo'
+                        src={slideshow8}
+                        alt="slideshow 8"
+                        />
+                    </div>
+                </div>
+            </div>
             </div>
             <div className='Slideshow2-container'>
                 <div className='photos'>
@@ -25,6 +39,16 @@ export function Slideshow() {
                         alt="Slideshow 2"
                     />
                 </div>
+                <div className= 'slideshow2-container'>
+                <div className='photo'>
+                    <img
+                    className='photo'
+                    src={slideshow7}
+                    alt="slideshow7">
+                        
+                    </img>
+                </div>
+            </div>
             </div>
             <div className='Slideshow4-container'>
                 <div className='photos'>
@@ -52,6 +76,7 @@ export function Slideshow() {
 
                     </img>
                 </div>
+            
             </div>
             </div>
             </div>
