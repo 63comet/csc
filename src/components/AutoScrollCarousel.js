f31e26859622dee3450913d06b2e47bbe2e4e00a
Lizbeth Lage <llage.bergen@gmail.com>
2025-01-08 12:02:42 -0500

    Add LiquidIV.png

--- src/components/AutoScrollCarousel.js
+++ src/components/AutoScrollCarousel.js
@@ -1,53 +1,35 @@
 import React from "react";
 import "../scss/AutoScrollCarousel.scss";
-import bay_st from "../assets/logos/Partners/bay_st.png";
-import EonsWhite from "../assets/logos/Partners/EonsWhite.png";
 import CampSnap from "../assets/logos/Partners/CampSnap.png";
-import CraversCircle from "../assets/logos/Partners/CraversCircle.png";
-import GoMacro from "../assets/logos/Partners/GoMacro.png";
-import Grove_Street_bikes from "../assets/logos/Partners/Grove_Street_bikes.png";
-import Knockaround from "../assets/logos/Partners/Knockaround.png";
 import MaskGroup from "../assets/logos/Partners/MaskGroup.png";
 import ONRunning from "../assets/logos/Partners/ONRunning.png";
 import RapidRebootBlack from "../assets/logos/Partners/RapidRebootBlack.png";
-import RiseCircle from "../assets/logos/Partners/RiseCircle.png";
 import SaltStickCircle from "../assets/logos/Partners/SaltStickCircle.png";
-import Winning_Events from "../assets/logos/Partners/Winning_Events.png";
 import Lululemon from "../assets/logos/Partners/Lululemon.png";
 import NewBalance from "../assets/logos/Partners/Newbalance.png";
 import LiquidIVWhite from "../assets/logos/Partners/LiquidIVWhite.png";
-import GNGRLabsWhite from "../assets/logos/Partners/GNGRLabsWhite.png";
 import LMNTWhite from "../assets/logos/Partners/LMNTWhite.png";
-import NaturesFyndWhite from "../assets/logos/Partners/NaturesFyndWhite.png";
-import SaludWhite from "../assets/logos/Partners/SaludWhite.png";
-import Spoonful from "../assets/logos/Partners/Spoonful.png";
 import StravaWhite from "../assets/logos/Partners/StravaWhite.png";
+import Thefeed from "../assets/logos/Partners/Thefeed.png";
+import Baystreet from "../assets/logos/Partners/Baystreet.png";
+import Winningsevents "../assets/logos/Partners/Winningsevents.png";
 
 export function AutoScrollCarousel() {
   // Define the logos array
   const logos = [
-    bay_st,
     CampSnap,
-    CraversCircle,
-    GoMacro,
-    Grove_Street_bikes,
-    Knockaround,
-    MaskGroup,
     ONRunning,
+    MaskGroup,
+    Baystreet,
     RapidRebootBlack,
-    RiseCircle,
+    Winningsevents,
     SaltStickCircle,
-    Winning_Events,
     Lululemon,
     NewBalance,
-    SaludWhite,
-    Spoonful,
     StravaWhite,
-    LMNTWhite,
     LiquidIVWhite,
-    GNGRLabsWhite,
-    EonsWhite,
-    NaturesFyndWhite
+    Thefeed,
+    LMNTWhite,
   ];
 
   return (
